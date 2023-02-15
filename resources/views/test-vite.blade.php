1838bf59d364348d6ee196ae80ff3e173dd5b314
Raihan Chowdhury <raihan92@rocketmail.com>
2023-02-15 23:22:22 +0600

    vite still not working

---
+++ resources/views/test-vite.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Test Vite</title>
+</head>
+<body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="bg-success">
+                    Hello World!!!
+                </div>
+            </div>
+        </div>
+    </div>
+
+    @vite(['resources/js/app.js'])
+</body>
+</html>
